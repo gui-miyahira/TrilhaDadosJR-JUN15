ff6452410d3d0cee593ac3957e889cc80aa09829
unknown <guilherme.miyahira@mathex.com.br>
2024-07-09 15:41:32 -0300

    Estatísticas Descritivas

--- trilhaCiencDados.py
+++ trilhaCiencDados.py
@@ -1,27 +1,32 @@
 #--Importando a biblioteca panda
+
 import pandas as pd 
 
 
-#--Inserindo os dados do arquivo CSV
+#--Carregamento de Dados: inserindo os dados do arquivo CSV
+
 tabela = pd.read_csv("texto.csv", sep=",")
 
 
-#--Exibindo as primeiras linhas do conjunto de dados;
+#--Exploração de Dados:
+#--exibindo as primeiras linhas do conjunto de dados
 #--número de linhas
 #--número de colunas
 #--tipos de dados da tabela
-
 
 print("\nPrimeiras 5 linhas do DataFrame: \n")
 print(tabela.head())
 
 print("\nNúmero de linhas:", tabela.shape[0])
 
-
 print("\nNúmero de colunas:", tabela.shape[1])
-
 
 print("\nTipos de dados da tabela:\n")
 print(tabela.dtypes,"\n")
+
+#--Estatísticas Descritivas: calcular e exibir descritivas básicas - média, mediana, mínimo, máximo, desvio padrão
+
+
+print(tabela.describe())
 
 
